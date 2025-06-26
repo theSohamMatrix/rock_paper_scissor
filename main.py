33ed258a0a870560795e518be5972df820060346
theSohamMatrix <Soham22022008@gmail.com>
2025-06-26 01:09:40 -0700

    intro to game

---
+++ main.py
@@ -0,0 +1,20 @@
+# Rock, Paper & Scissor Game ...
+'''
+Choices for computer: 
+1 Rock = 1 
+2 Paper = 0
+3 Scissor = -1
+'''
+import random
+# Header of game
+print("="*50)
+print("Rock Paper & Scissor Game".center(50))
+print("="*50)
+
+# User choices: 
+print('''Choose from this options: 
+      1- Rock    : R
+      2- Paper   : P
+      3- Scissor : S
+      '''
+)
